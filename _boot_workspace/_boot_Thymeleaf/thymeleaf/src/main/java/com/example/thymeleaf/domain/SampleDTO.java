d2df500d294b96938fd39753a63a23117b6cac28
gwesgaw <143565379+gwesgaw@users.noreply.github.com>
2024-01-22 15:13:29 +0900

    day01

---
+++ _boot_workspace/_boot_Thymeleaf/thymeleaf/src/main/java/com/example/thymeleaf/domain/SampleDTO.java
@@ -0,0 +1,16 @@
+package com.example.thymeleaf.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class SampleDTO {
+	
+	private String p1, p2, p3;
+
+}
